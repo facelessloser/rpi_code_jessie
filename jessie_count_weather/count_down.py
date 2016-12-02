6a9cae5145c7e6fca9e7863062a0963d0e5af38d
facelessloser <facelessloser@gmail.com>
2016-12-02 11:30:30 +0000

    commited out test

--- jessie_count_weather/count_down.py
+++ jessie_count_weather/count_down.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #!/usr/bin/env
  
-import urllib2, json, os
+import urllib2, json, os, time
 import RPi.GPIO as GPIO
  
 import datetime
@@ -9,52 +9,52 @@ from time import strftime
  
  
 # ---- ws2812 stuff ----
-import time
-from neopixel import *
- 
-# LED strip configuration:
-LED_COUNT = 8 # Number of LED pixels.
-LED_PIN = 12 # GPIO pin connected to the pixels (must support PWM!) 
-LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 5 # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 127 # Set to 0 for darkest and 255 for brightest
-#LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
- 
-# ---- ws2812 stuff ----
- 
- 
-# ---- Screen stuff ----
- 
-import Adafruit_SSD1306
-import Image
-import ImageDraw
-import ImageFont
- 
-# Raspberry Pi pin configuration:
-RST = 24
- 
-# 128x64 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
- 
-# Initialize library.
-disp.begin()
- 
-# Clear display.
-disp.clear()
-disp.display()
-   
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-  
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-#font = ImageFont.load_default()
-#font = ImageFont.truetype('Minecraftia-Regular.ttf', 12)
-font = ImageFont.truetype('keep.ttf', 14)
+#import time
+#from neopixel import *
+# 
+## LED strip configuration:
+#LED_COUNT = 8 # Number of LED pixels.
+#LED_PIN = 12 # GPIO pin connected to the pixels (must support PWM!) 
+#LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+#LED_DMA = 5 # DMA channel to use for generating signal (try 5)
+#LED_BRIGHTNESS = 127 # Set to 0 for darkest and 255 for brightest
+##LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
+#LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
+# 
+## ---- ws2812 stuff ----
+# 
+# 
+## ---- Screen stuff ----
+# 
+#import Adafruit_SSD1306
+#import Image
+#import ImageDraw
+#import ImageFont
+# 
+## Raspberry Pi pin configuration:
+#RST = 24
+# 
+## 128x64 display with hardware I2C:
+#disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+# 
+## Initialize library.
+#disp.begin()
+# 
+## Clear display.
+#disp.clear()
+#disp.display()
+#   
+## Create blank image for drawing.
+## Make sure to create image with mode '1' for 1-bit color.
+#width = disp.width
+#height = disp.height
+#image = Image.new('1', (width, height))
+#  
+## Get drawing object to draw on image.
+#draw = ImageDraw.Draw(image)
+##font = ImageFont.load_default()
+##font = ImageFont.truetype('Minecraftia-Regular.ttf', 12)
+#font = ImageFont.truetype('keep.ttf', 14)
 # ---- Screen stuff ----
  
 #GPIO.setmode(GPIO.BOARD)
