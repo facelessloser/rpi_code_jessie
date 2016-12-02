e54ac5ba349dc788389cd2a8f587ed53cfac93f9
facelessloser <facelessloser@gmail.com>
2016-12-02 12:09:49 +0000

    Cleaned up code

--- jessie_count_weather/count_down.py
+++ jessie_count_weather/count_down.py
@@ -1,25 +1,15 @@
-# shutdown/reboot(/power on) Raspberry Pi with pushbutton
-# Button connected to pin 15
-# LED connected to pin 16
-
 import RPi.GPIO as GPIO
 from subprocess import call
 import time, os
 
-#shutdownPin = 15 
-
 
 GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(shutdownPin, GPIO.IN)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def buttonStateChanged(pin):
 
     if not (GPIO.input(pin)):
-        #print"button press"
-        #GPIO.output(16, True) # Turn on pin 16 (LED)
-        #print"Shutdown"
         os.system("sudo python xmas_count.py")
 
 GPIO.add_event_detect(11, GPIO.BOTH, callback=buttonStateChanged)
